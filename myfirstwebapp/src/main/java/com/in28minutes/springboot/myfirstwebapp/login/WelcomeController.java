d4231c8d0e953bc09e3baf5f217dcc53affea1ce
Trickylime <jackmcgeown@yahoo.co.uk>
2024-02-22 20:53:35 +0000

    Web Application With Spring Boot

---
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/WelcomeController.java
@@ -0,0 +1,46 @@
+package com.in28minutes.springboot.myfirstwebapp.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("name")
+public class WelcomeController {
+
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String gotoWelcomePage(ModelMap model) {//@RequestParam String name, ModelMap model
+
+        model.put("name", "Jack");
+        return "welcome";
+    }
+
+//    private Logger logger = LoggerFactory.getLogger(getClass());
+
+//    private AuthenticationService authenticationService;
+//
+//    public LoginController(AuthenticationService authenticationService) {
+//        this.authenticationService = authenticationService;
+//    }
+//    @RequestMapping(value = "login", method = RequestMethod.POST)
+//    public String gotoWelcomePage(@RequestParam String name, @RequestParam String password,
+//                                  ModelMap model) {
+//
+//        if (authenticationService.authenticate(name, password)) {
+//            model.put("name", name);
+//
+//            return "welcome";
+//        }
+//
+//        model.put("errorMessage", "Invalid Credentials! Please try again.");
+//        return "login";
+//    }
+
+//        logger.debug("Request param is {}", name);
+//        logger.info("I want this printed at info level");
+//        logger.warn("I want this printed at warn level");
+//        System.out.printf("Request param is %s%n", name);//NOT RECOMMENDED FOR PROD CODE
+}
