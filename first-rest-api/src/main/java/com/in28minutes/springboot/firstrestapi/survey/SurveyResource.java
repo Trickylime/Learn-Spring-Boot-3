2d6d17ac86105a3c8c90b04b8f7c034c9b25e2f8
Trickylime <jackmcgeown@yahoo.co.uk>
2024-03-07 19:47:34 +0000

    Spring Boot Deep Dive With REST API

---
+++ first-rest-api/src/main/java/com/in28minutes/springboot/firstrestapi/survey/SurveyResource.java
@@ -0,0 +1,71 @@
+package com.in28minutes.springboot.firstrestapi.survey;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class SurveyResource {
+
+    private SurveyService surveyService;
+
+
+    public SurveyResource(SurveyService surveyService) {
+        this.surveyService = surveyService;
+    }
+
+    @RequestMapping("/surveys")
+    public List<Survey> retrieveAllSurveys() {
+        return surveyService.retrieveAllSurveys();
+    }
+
+    @RequestMapping("/surveys/{surveyId}")
+    public Survey retrieveSurveyById(@PathVariable String surveyId) {
+
+       Survey survey = surveyService.retrieveSurveyById(surveyId);
+
+       if (survey == null)
+           throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+       return survey;
+    }
+
+    @RequestMapping("/surveys/{surveyId}/questions")
+    public List<Question> retrieveAllSurveyQuestions(@PathVariable String surveyId) {
+
+        List<Question> questions = surveyService.retrieveAllSurveyQuestions(surveyId);
+
+        if (questions == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        return questions;
+    }
+
+    @RequestMapping("/surveys/{surveyId}/questions/{questionId}")
+    public Question retrieveQuestionById(@PathVariable String surveyId,
+                                     @PathVariable String questionId) {
+
+        Question questions = surveyService.retrieveQuestionById(surveyId, questionId);
+
+        if (questions == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        return questions;
+    }
+
+    @RequestMapping(value="/surveys/{surveyId}/questions", method = RequestMethod.POST)
+    public ResponseEntity<Object> addNewSurveyQuestions(@PathVariable String surveyId, @RequestBody Question question) {
+
+        String questionId = surveyService.addNewSurveyQuestion(surveyId, question);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{questionId}").buildAndExpand(questionId).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+}
